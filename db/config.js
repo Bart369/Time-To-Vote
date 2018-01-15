adda7956ec680b2479bb9e7109824e859d54178f
bart369 <bartinpeguero@gmail.com>
2018-01-14 23:12:39 -0500

    changes to config for heroku

--- db/config.js
+++ db/config.js
@@ -6,20 +6,21 @@ const options = {
 
 const pgp = require('pg-promise')(options)
 
+let db
 
 
-function setDatabase(){
-    if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
-        return pgp({
-            database: 'timetovote',
-            port: 5432,
-            host: 'localhost'
-        })
-    }   else if (process.env.NODE_ENV === 'production'){
-            return pgp(process.env.DATABASE_URL)
-    }
+if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+    db = pgp({
+        database: 'timetovote',
+        port: 5432,
+        host: 'localhost'
+    })
+}   else if (process.env.NODE_ENV === 'production'){
+        db = pgp(process.env.DATABASE_URL);
+        // return pgp(process.env.DATABASE_URL)
 }
 
-const db = setDatabase()
+
+
 
 module.exports = db
